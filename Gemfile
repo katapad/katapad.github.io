98f919dbb2144e0f7860db84be8a59a104bf86fc
katapad <katapad@gmail.com>
2014-06-08 11:59:01 +0900

    middleman-blog -template=blogで初期化しなおした

--- Gemfile
+++ Gemfile
@@ -1,11 +1,20 @@
+# If you have OpenSSL installed, we recommend updating
+# the following line to use "https"
 source 'http://rubygems.org'
 
-gem "middleman", "~>3.3.3"
-gem "slim", "~> 2.0.0"
+gem "middleman", "~> 3.3.3"
+gem "middleman-blog", "~> 3.5.3"
+
+# For feed.xml.builder
+gem "builder", "~> 3.0"
+
+
+
+source 'http://rubygems.org'
+
+gem "slim", "~> 2.0.2"
 gem "middleman-livereload", "~> 3.3.3"
 gem "middleman-deploy", "~> 0.2.4"
-
+gem "middleman-syntax", "~> 2.0.0"
 
 gem "redcarpet", "~> 3.1.2"
-gem "middleman-syntax", "~> 2.0.0"
-gem "middleman-blog", "~> 3.5.3"
--- config.rb
+++ config.rb
@@ -1,10 +1,50 @@
+# Slim settings
+# Set slim-lang output style
+Slim::Engine.set_default_options :pretty => true
+# Set Shortcut
+Slim::Engine.set_default_options :shortcut => {
+    '#' => {:tag => 'div', :attr => 'id'},
+    '.' => {:tag => 'div', :attr => 'class'},
+    '&' => {:tag => 'input', :attr => 'type'}
+}
+
+
+###
+# Blog settings
+###
+
+Time.zone = "Tokyo"
+
+activate :blog do |blog|
+  # This will add a prefix to all links, template references and source paths
+  # blog.prefix = "posts"
+
+  # blog.permalink = "{year}/{month}/{day}/{title}.html"
+  # Matcher for blog source files
+  blog.sources = "posts/{year}-{month}-{day}-{title}.html"
+  # blog.taglink = "tags/{tag}.html"
+  # blog.layout = "layout"
+  blog.summary_separator = /(READMORE)/
+  # blog.summary_length = 250
+  # blog.year_link = "{year}.html"
+  # blog.month_link = "{year}/{month}.html"
+  # blog.day_link = "{year}/{month}/{day}.html"
+  blog.default_extension = ".md"
+
+  blog.tag_template = "tag.html"
+  blog.calendar_template = "calendar.html"
+
+  # Enable pagination
+  # blog.paginate = true
+  # blog.per_page = 10
+  # blog.page_link = "page/{num}"
+end
+
+page "/feed.xml", layout: false
+
 ###
 # Compass
 ###
-
-# Susy grids in Compass
-# First: gem install susy --pre
-# require 'susy'
 
 # Change Compass configuration
 # compass_config do |config|
@@ -15,38 +55,22 @@
 # Page options, layouts, aliases and proxies
 ###
 
-# Slim settings
-# Set slim-lang output style
-Slim::Engine.set_default_options :pretty => true
-# Set Shortcut
-Slim::Engine.set_default_options :shortcut => {
-  '#' => {:tag => 'div', :attr => 'id'},
-  '.' => {:tag => 'div', :attr => 'class'},
-  '&' => {:tag => 'input', :attr => 'type'}
-}
-
-# bower.js settings
-set :bower_dir, '../vendor/bower'
-
-# activate :gzip
-
 # Per-page layout changes:
 #
 # With no layout
-# page "/path/to/file.html", :layout => false
+# page "/path/to/file.html", layout: false
 #
 # With alternative layout
-# page "/path/to/file.html", :layout => :otherlayout
+# page "/path/to/file.html", layout: :otherlayout
 #
 # A path which all have the same layout
 # with_layout :admin do
 #   page "/admin/*"
 # end
 
-# Proxy (fake) files
-# page "/this-page-has-no-template.html", :proxy => "/template-file.html" do
-#   @which_fake_page = "Rendering a fake page with a variable"
-# end
+# Proxy pages (http://middlemanapp.com/basics/dynamic-pages/)
+# proxy "/this-page-has-no-template.html", "/template-file.html", locals: {
+#  which_fake_page: "Rendering a fake page with a local variable" }
 
 ###
 # Helpers
@@ -54,6 +78,7 @@ set :bower_dir, '../vendor/bower'
 
 # Automatic image dimensions on image_tag helper
 activate :automatic_image_sizes
+activate :livereload
 
 # Methods defined in the helpers block are available in templates
 # helpers do
@@ -62,39 +87,15 @@ activate :automatic_image_sizes
 #   end
 # end
 
-# set file names
 set :css_dir, 'css'
 set :js_dir, 'js'
 set :images_dir, 'img'
 
-# livereload your browser, Firefox/Google Chrome/Safari
-activate :livereload
-
-
-# blog
-Time.zone = 'Tokyo'
-
-activate :blog do |blog|
-  # ブログ機能のオプションを設定
-  blog.sources = "posts/:year-:month-:day-:title.html"
-  blog.permalink = ":year/:month/:title.html"
-  blog.default_extension = ".md"
-end
-activate :directory_indexes
-page "/feed.xml", :layout => false
-
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
   activate :minify_javascript
-
-  # Enable cache buster
   activate :cache_buster
-
-  # Use relative URLs
   activate :relative_assets
 
   # Compress PNGs after build
@@ -104,33 +105,4 @@ configure :build do
 
   # Or use a different image path
   # set :http_path, "/Content/images/"
-end
-
-# Deploy-specific configuration
-activate :deploy do |deploy|
-  deploy.build_before = true # default: false
-
-  # git
-  deploy.method = :git
-  # Optional Settings
-  # deploy.remote = "custom-remote" # remote name or git url, default: origin
-  # deploy.branch = "gh-pages" # default: gh-pages
-
-  # ftp
-  # deploy.method   = :ftp
-  # deploy.host     = "ftp.example.com"
-  # deploy.user     = "tvaughan"
-  # deploy.password = "secret"
-  # deploy.path     = "/srv/www/site"
-
-  # sftp
-  # require 'json'
-  # json = JSON.load(File.read('.ftppass'))
-  # ftp = json['test']
-  #
-  # deploy.method   = :sftp
-  # deploy.host     = ftp['host']
-  # deploy.user     = ftp['user']
-  # deploy.password = ftp['password']
-  # deploy.path     = ftp['remotePathi']
 end
